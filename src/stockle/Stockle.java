960a367173089abeee2938e124c0e1fd7ad2548e
Isaac Bock <isaactbock@wustl.edu>
2022-04-25 13:04:01 -0500

    Stylized output w/ bold & italics

--- src/stockle/Stockle.java
+++ src/stockle/Stockle.java
@@ -146,6 +146,7 @@ public class Stockle {
 		//compare each attribute of the guess to the attribute of the correct answer
 		System.out.println("--------------------------------------------------------");
 		System.out.println("You guessed " + userGuessCompany.getSymbol() + ".");
+		System.out.println("𝐛𝐨𝐥𝐝=𝐜𝐨𝐫𝐫𝐞𝐜𝐭; 𝘪𝘵𝘢𝘭𝘪𝘤𝘴=𝘤𝘭𝘰𝘴𝘦; regular=incorrect");
 		System.out.println();
 		compareIndustry(userGuessCompany, answer);
 		compareMarketCaps(userGuessCompany, answer);
@@ -157,111 +158,106 @@ public class Stockle {
 	}
 	
 	public boolean compareIndustry(Company userGuessCompany, Company answer) {
-		System.out.print("Industry: ");
 		if (userGuessCompany.getSector().equals(answer.getSector())) {
 			if (userGuessCompany.getIndustry().equals(answer.getIndustry())) {
-				System.out.println("Your guess is in the same industry as the target");
+				System.out.println(String.format("𝐈𝐧𝐝𝐮𝐬𝐭𝐫𝐲: 𝐂𝐨𝐫𝐫𝐞𝐜𝐭! (%s)", userGuessCompany.getIndustry()));
 				return true;
 			} else {
-				System.out.println("Getting there! You've got the right sector but the wrong industry");
+				System.out.println(String.format("𝘐𝘯𝘥𝘶𝘴𝘵𝘳𝘺: 𝘎𝘦𝘵𝘵𝘪𝘯𝘨 𝘵𝘩𝘦𝘳𝘦! 𝘠𝘰𝘶'𝘷𝘦 𝘨𝘰𝘵 𝘵𝘩𝘦 𝘳𝘪𝘨𝘩𝘵 𝘴𝘦𝘤𝘵𝘰𝘳 𝘣𝘶𝘵 𝘵𝘩𝘦 𝘸𝘳𝘰𝘯𝘨 𝘪𝘯𝘥𝘶𝘴𝘵𝘳𝘺 (%s)", userGuessCompany.getIndustry()));
 				return true;
 			}
 			}
 		else {
-			System.out.println("Keep trying! Wrong sector");
+			System.out.println(String.format("Industry: Keep trying! Wrong sector and industry... (%s)", userGuessCompany.getIndustry()));
 			return false;
 		}
 	}
 	
 	public boolean compareMarketCaps(Company userGuessCompany, Company answer) {
-		System.out.print("Market Cap: ");
 		long fivePercentUp = (long) (answer.getMarketCap() * 1.05);
 		long fivePercentDown = (long) (answer.getMarketCap() * 0.95);
+		long oneBillion = 1000000000L;
 		if (userGuessCompany.getMarketCap() == answer.getMarketCap()) {
-			System.out.println("Correct market cap!");
+			System.out.println(String.format("𝐌𝐚𝐫𝐤𝐞𝐭 𝐂𝐚𝐩: 𝐂𝐨𝐫𝐫𝐞𝐜𝐭! ($%d billion)", userGuessCompany.getMarketCap()/oneBillion));
 			return true;
 		}
 		if (fivePercentUp >= userGuessCompany.getMarketCap() && fivePercentDown <= userGuessCompany.getMarketCap()) {
-			System.out.println("Your guess is within 5% of the target company's!");
+			System.out.println(String.format("𝘔𝘢𝘳𝘬𝘦𝘵 𝘊𝘢𝘱: 𝘊𝘭𝘰𝘴𝘦! 𝘠𝘰𝘶𝘳 𝘨𝘶𝘦𝘴𝘴 𝘪𝘴 𝘸𝘪𝘵𝘩𝘪𝘯 5%% 𝘰𝘧 𝘵𝘩𝘦 𝘵𝘢𝘳𝘨𝘦𝘵 𝘤𝘰𝘮𝘱𝘢𝘯𝘺'𝘴 ($%d billion)!", userGuessCompany.getMarketCap()/oneBillion));
 			return true;
 		}
 		else {
-			System.out.println("Your guess is not within 5% of the target company's market cap");
+			System.out.println(String.format("Market Cap: Your guess is not within 5%% of the target... ($%d billion)", userGuessCompany.getMarketCap()/oneBillion));
 			return false;
 		}
 	}
 	
 	public boolean compareSizes(Company userGuessCompany, Company answer) {
-		System.out.print("Size: ");
 		if (userGuessCompany.getSize().equals(answer.getSize())) {
-			System.out.println("Correct size!");
+			System.out.println(String.format("𝐒𝐢𝐳𝐞: 𝐂𝐨𝐫𝐫𝐞𝐜𝐭! (%s)", userGuessCompany.getSize()));
 			return true;
 		}
 		else {
-			System.out.println("Incorrect size...");
+			System.out.println(String.format("Size: Incorrect... (%s)", userGuessCompany.getSize()));
 			return false;
 		}
 	}
 	
 	public boolean compareHeadquarters(Company userGuessCompany, Company answer) {
-		System.out.print("Headquarters: ");
 		if (userGuessCompany.getCountry().equals(answer.getCountry())) {
 			if (userGuessCompany.getCountry().contains("United States")) {
 				if (userGuessCompany.getHeadquarters().equals(answer.getHeadquarters())) {
-					System.out.println("Correct city!");
+					System.out.println(String.format("𝐇𝐞𝐚𝐝𝐪𝐮𝐚𝐫𝐭𝐞𝐫𝐬: 𝐂𝐨𝐫𝐫𝐞𝐜𝐭 𝐜𝐢𝐭𝐲! (%s)", userGuessCompany.getHeadquarters()));
 					return true;
 				} else if (userGuessCompany.getState().equals(answer.getState())) {
-					System.out.println("Correct state!");
+					System.out.println(String.format("𝐇𝐞𝐚𝐝𝐪𝐮𝐚𝐫𝐭𝐞𝐫𝐬: 𝐂𝐨𝐫𝐫𝐞𝐜𝐭 𝐬𝐭𝐚𝐭𝐞! (%s)", userGuessCompany.getState()));
 					return true;
 				} else {
-					System.out.println("Correct country!");
+					System.out.println(String.format("𝐇𝐞𝐚𝐝𝐪𝐮𝐚𝐫𝐭𝐞𝐫𝐬: 𝐂𝐨𝐫𝐫𝐞𝐜𝐭 𝐜𝐨𝐮𝐧𝐭𝐫𝐲! (%s)", userGuessCompany.getCountry()));
 					return true;
 				}
 			} else {
-				System.out.println("Correct country!");
+				System.out.println(String.format("𝐇𝐞𝐚𝐝𝐪𝐮𝐚𝐫𝐭𝐞𝐫𝐬: 𝐂𝐨𝐫𝐫𝐞𝐜𝐭 𝐜𝐨𝐮𝐧𝐭𝐫𝐲! (%s)", userGuessCompany.getCountry()));
 				return true;
 			}
 		}
-		System.out.println("Wrong country!");
+		System.out.println(String.format("Headquarters: Wrong country... (%s)", userGuessCompany.getCountry()));
 		return false;
 		
 	}
 	
 	public boolean compareYearsFounded(Company userGuessCompany, Company answer) {
-		System.out.print("Year Founded: ");
 		int upperBound = answer.getYearFounded() + 10;
 		int lowerBound = answer.getYearFounded() - 10;
 		if (userGuessCompany.getYearFounded() == answer.getYearFounded()) {
-			System.out.println("Correct year founded!");
+			System.out.println(String.format("𝐘𝐞𝐚𝐫 𝐅𝐨𝐮𝐧𝐝𝐞𝐝: 𝐂𝐨𝐫𝐫𝐞𝐜𝐭! (%d)", userGuessCompany.getYearFounded()));
 			return true;
 		}
 		if (upperBound >= userGuessCompany.getYearFounded() && lowerBound <= userGuessCompany.getYearFounded()) {
-			System.out.println("Guess is within 10 years of answer!");
+			System.out.println(String.format("𝘠𝘦𝘢𝘳 𝘍𝘰𝘶𝘯𝘥𝘦𝘥: 𝘊𝘭𝘰𝘴𝘦! 𝘞𝘪𝘵𝘩𝘪𝘯 10 𝘺𝘦𝘢𝘳𝘴 𝘰𝘧 𝘢𝘯𝘴𝘸𝘦𝘳! (%d)", userGuessCompany.getYearFounded()));
 			return true;
 		}
 		else {
-			System.out.println("Guess was founded outside of 10 years near target");
+			System.out.println(String.format("Year Founded: 10+ years outside of target... (%d)", userGuessCompany.getYearFounded()));
 			return false;
 		}
 	}
 	
 	public boolean compareOneYearReturns(Company userGuessCompany, Company answer) {
-		System.out.print("One Year Return: ");
 		int upperBound = (int) (answer.getOneYearReturn() + 10);
 		int lowerBound = (int) (answer.getOneYearReturn() - 10);
 		
 		if (userGuessCompany.getOneYearReturn() == answer.getOneYearReturn()) {
-			System.out.println("Correct one year return!");
+			System.out.println(String.format("𝐎𝐧𝐞 𝐘𝐞𝐚𝐫 𝐑𝐞𝐭𝐮𝐫𝐧: 𝐂𝐨𝐫𝐫𝐞𝐜𝐭! (%.2f%%)", userGuessCompany.getOneYearReturn()));
 			System.out.println(" ");
 			return true;
 		}
 		if (upperBound >= userGuessCompany.getOneYearReturn() && lowerBound <= userGuessCompany.getOneYearReturn()) {
-			System.out.println("Guess is within 10% answer!");
+			System.out.println(String.format("𝘖𝘯𝘦 𝘠𝘦𝘢𝘳 𝘙𝘦𝘵𝘶𝘳𝘯: 𝘞𝘪𝘵𝘩𝘪𝘯 10%% 𝘰𝘧 𝘢𝘯𝘴𝘸𝘦𝘳! (%.2f%%)", userGuessCompany.getOneYearReturn()));
 			System.out.println(" ");
 			return true;
 		}
 		else {
-			System.out.println("Incorrect one year return...");
+			System.out.println(String.format("One Year Return: Incorrect... (%.2f%%)", userGuessCompany.getOneYearReturn()));
 			System.out.println(" ");
 			return false;
 		}
